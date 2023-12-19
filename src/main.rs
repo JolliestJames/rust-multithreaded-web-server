69887653ddaf2fc444e4c981add9e1e2e34b2b92
jame <cloudismydog@gmail.com>
2023-12-19 10:53:53 -0800

    handle 404s

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     if request_line == "GET / HTTP/1.1" {
         let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("src/hello.html").unwrap();
+        let contents = fs::read_to_string("hello.html").unwrap();
         let length = contents.len();
 
         let response =
@@ -29,6 +29,14 @@ fn handle_connection(mut stream: TcpStream) {
 
         stream.write_all(response.as_bytes()).unwrap();
     } else {
-        //some other request
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
