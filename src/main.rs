5dedca99c2de0162fd7207e4be921cfbbc0c04d5
jame <cloudismydog@gmail.com>
2023-12-19 15:32:24 -0800

    use an Arc and a Mutex to share and do work with consumer safely across threads

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,33 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    sync::{mpsc, Mutex, Arc},
     thread,
     time::Duration,
 };
 
 pub struct ThreadPool{
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+pub struct Job;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -35,8 +43,10 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
 
         Worker { id, thread }
     }
